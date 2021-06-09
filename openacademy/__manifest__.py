3ea829c8a077e1ce4f838f63dc69fabba21ad586
Hector Gutierrez <78827908+gutierrezhector24@users.noreply.github.com>
2021-06-09 16:52:08 -0600

    [ADD] menu, access

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -24,8 +24,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views.xml',
+        'security/ir.model.access.csv',
+        'views/openacademy.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
