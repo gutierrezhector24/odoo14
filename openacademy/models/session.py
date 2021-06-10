9968cbe4de428844f0480a2571780c95db9efe7e
Hector Gutierrez <78827908+gutierrezhector24@users.noreply.github.com>
2021-06-10 12:45:17 -0600

    [ADD] basic views +

---
+++ openacademy/models/session.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from odoo import models, fields, api
+
+class session(models.Model):
+    _name = 'openacademy.session'
+    _description = "OpenAcademy Sessions"
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6, 2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Ïnstructor",
+        domain=['|', ('instructor', '=', True),('category_id.name', 'ilike', "Teacher")])
+
+    course_id = fields.Many2one('openacademy.course', 
+        ondelete='cascade', string="Course", required=True)
+    attendee_ids = fields.Many2many('res.partner', string="Attendees")
+    
